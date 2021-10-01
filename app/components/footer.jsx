95d0ee31b84b81048cc484b8cf0bec664e6ce226
intai <intai.hg@gmail.com>
2021-10-02 09:43:24 +1300

    Add info in footer

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -3,39 +3,76 @@ import styled from 'styled-components'
 import { textOffWhite, backgroundGrey } from './color'
 import { fontSerif } from './typography'
 
-const Container = styled.div`
+const Container = styled.footer`
   ${backgroundGrey}
   ${textOffWhite}
   ${fontSerif}
-  padding: 10px 0;
-  text-align: center;
+`
+
+const Section = styled.section`
+  display: inline-block;
+  text-align: left;
+  vertical-align: top;
+  margin: 20px 0 0 20px;
+
+  :last-child {
+    margin-right: 20px;
+    margin-bottom: 20px;
+  }
+`
+
+const Title = styled.div`
+  font-size: 150%;
+  margin-bottom: 20px;
+`
+
+const Line = styled.div`
+  font-size: 100%;
+`
+
+const LineBreak = styled(Line)`
+  margin-bottom: 10px;
+`
+
+const Anchor = styled.a`
+  display: block;
 `
 
 const Footer = () => (
   <Container>
-    {'Icons made by '}
-    <a
-      href="https://www.flaticon.com/authors/iconnice"
-      title="Iconnice"
-    >
-      {'Iconnice'}
-    </a>
-    {' from '}
-    <a
-      href="https://www.flaticon.com/"
-      title="Flaticon"
-    >
-      {'www.flaticon.com'}
-    </a>
-    {' is licensed by '}
-    <a
-      href="http://creativecommons.org/licenses/by/3.0/"
-      rel="noopener noreferrer"
-      target="_blank"
-      title="Creative Commons BY 3.0"
-    >
-      {'CC 3.0 BY'}
-    </a>
+    <Section>
+      <Title>{'Opening Hours'}</Title>
+      <Line>{'Mon Closed'}</Line>
+      <Line>{'Tue-Sat 10am - 4pm'}</Line>
+      <Line>{'Sunday & Public Holidays Closed'}</Line>
+      <Line>{'Book an appointment for consultation'}</Line>
+    </Section>
+    <Section>
+      <Title>{'Contact'}</Title>
+      <Line>{'info@xianbridal.co.nz'}</Line>
+      <LineBreak>{'021-1409204'}</LineBreak>
+      <Line>{'Shop 5 New Lynn Plaza'}</Line>
+      <Line>{'3115 Great North Rd'}</Line>
+      <Line>{'New Lynn'}</Line>
+      <Line>{'Auckland'}</Line>
+    </Section>
+    <Section>
+      <Title>{'Social'}</Title>
+      <Anchor
+        href="https://www.facebook.com/xianbridal"
+        rel="noreferrer"
+        target="_blank"
+      >
+        {'Facebook'}
+      </Anchor>
+      <Anchor
+        href="https://www.instagram.com/xianbridal"
+        rel="noreferrer"
+        target="_blank"
+      >
+        {'Instagram'}
+      </Anchor>
+    </Section>
   </Container>
 )
 
