78f89921e73bc5517bff80a49c2429ee8ad3e689
intai <intai.hg@gmail.com>
2021-10-02 14:42:10 +1300

    Remove web cdn from env

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -14,7 +14,7 @@ module.exports = {
     new webpack.NoEmitOnErrorsPlugin(),
     new webpack.DefinePlugin({
       IMAGES_CDN_DOMAIN: JSON.stringify(process.env.IMAGES_CDN_DOMAIN || '/static/images'),
-      WEB_CDN_DOMAIN: JSON.stringify(`${process.env.WEB_CDN_DOMAIN || ''}`),
+      WEB_CDN_DOMAIN: JSON.stringify(process.env.WEB_CDN_DOMAIN || ''),
     }),
     new HtmlWebpackPlugin({
       filename: 'index.html',
