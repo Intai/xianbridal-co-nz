756162697fc80a557f7e6d85c98938a6b3a132cb
intai <intai.hg@gmail.com>
2021-10-05 23:01:37 +1300

    Setup cdn for images.xianbridal.co.nz

--- create-invalidation.js
+++ create-invalidation.js
@@ -3,30 +3,45 @@ const codedeploy = new AWS.CodeDeploy({ apiVersion: '2014-10-06' })
 const cloudfront = new AWS.CloudFront({ apiVersion: '2020-05-31' })
 
 exports.handler = (event, context, callback) => {
-  console.log('create invalidation', { event })
+  console.log('create invalidation', {
+    event,
+  })
+
   cloudfront.createInvalidation({
     DistributionId: 'E2PEUF278GG617',
     InvalidationBatch: {
       CallerReference: event.DeploymentId,
       Paths: {
         Quantity: 1,
-        Items: ['*'],
+        Items: [
+          '/*',
+        ],
       },
     },
   }, (errCdn, errData) => {
-    console.log('put lifecycle event hook', { errCdn, errData })
-    codedeploy.putLifecycleEventHookExecutionStatus({
-      deploymentId: event.DeploymentId,
-      lifecycleEventHookExecutionId: event.LifecycleEventHookExecutionId,
-      status: errCdn ? 'Failed' : 'Succeeded',
-    }, (errDeploy, dataDeploy) => {
-      console.log('finished validation', { errDeploy, dataDeploy })
-      if (errDeploy) {
-        callback('Validation test failed')
-      } else {
-        callback(null, 'Validation test succeeded')
-      }
+    console.log('finish invalidation', {
+      errCdn,
+      errData,
     })
+  })
+
+  console.log('put lifecycle event hook')
+
+  codedeploy.putLifecycleEventHookExecutionStatus({
+    deploymentId: event.DeploymentId,
+    lifecycleEventHookExecutionId: event.LifecycleEventHookExecutionId,
+    status: 'Succeeded',
+  }, (errDeploy, dataDeploy) => {
+    console.log('finish putting lifecycle', {
+      errDeploy,
+      dataDeploy,
+    })
+
+    if (errDeploy) {
+      callback('validation test failed')
+    } else {
+      callback(null, 'validation test succeeded')
+    }
   })
 
   return context.logStreamName
