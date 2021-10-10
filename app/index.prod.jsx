3dbb17ce28e4a8143e7ad33f8357858a9b9cdeae
intai <intai.hg@gmail.com>
2021-10-10 23:01:55 +1300

    Comment out gtag for now

--- app/index.prod.jsx
+++ app/index.prod.jsx
@@ -8,15 +8,15 @@ if ('serviceWorker' in navigator) {
   })
 }
 
-setTimeout(() => {
-  const script = document.createElement('script')
-  script.defer = true
-  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-9WZGHKT91D'
-  document.head.appendChild(script)
-
-  window.dataLayer = window.dataLayer || []
-  function gtag() { window.dataLayer.push(arguments) }
-  gtag('js', new Date())
-  gtag('config', 'G-M60CFNHRLK')
-  gtag('config', 'AW-980617762')
-}, 1000)
+// setTimeout(() => {
+//   const script = document.createElement('script')
+//   script.defer = true
+//   script.src = 'https://www.googletagmanager.com/gtag/js?id=G-9WZGHKT91D'
+//   document.head.appendChild(script)
+//
+//   window.dataLayer = window.dataLayer || []
+//   function gtag() { window.dataLayer.push(arguments) }
+//   gtag('js', new Date())
+//   gtag('config', 'G-M60CFNHRLK')
+//   gtag('config', 'AW-980617762')
+// }, 1000)
