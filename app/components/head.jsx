effc58fdb29bf2499e3efd4d28f0862c32f0af96
intai <intai.hg@gmail.com>
2022-03-26 15:01:23 +1300

    Add different title for category and search result

--- app/components/head.jsx
+++ app/components/head.jsx
@@ -34,26 +34,73 @@ const renderHead = ({ description, keywords, title }) => {
   )
 }
 
+const HeadSearchResult = (props) => {
+  const { match: { params: { query } } } = props
+  const { state } = useBdux(props, { catalogue: CatalogueStore })
+  const { catalogue } = state
+  const product = catalogue?.selected
+
+  if (!query) {
+    return false
+  }
+  if (product) {
+    return <HeadProduct {...props} />
+  }
+  return renderHead({
+    description: `Search result for "${query}" in Wedding Gowns, Sample Sales and Bridal Accessories`,
+    keywords: `Wedding,Dress,Gown,Veil,Tiara,${query}`,
+    title: `Xian Bridal: Search result for "${query}"`,
+  })
+}
+
+const categoryDescriptions = {
+  gowns: 'Wedding Gowns',
+  sales: 'Sample Sales',
+  accessories: 'Bridal Accessories',
+}
+
 const HeadProduct = (props) => {
   const { state } = useBdux(props, { catalogue: CatalogueStore })
   const { catalogue } = state
-  const product = catalogue && catalogue.selected
+  const product = catalogue?.selected
 
   if (!product) {
     return false
   }
   const sku = encodeSku(product.id)
+  const category = categoryDescriptions[product.category]
   return renderHead({
-    description: `Photos and details of Wedding Gown Item #${sku}. ${product.description}`,
+    description: `Photos and details of ${category} #${sku}. ${product.description}`,
     keywords: `#${sku},${product.keywords || ''}`,
-    title: `Xian Bridal: Wedding Dress Item #${sku}`,
+    title: `Xian Bridal: ${category} #${sku}`,
+  })
+}
+
+const HeadCategory = (props) => {
+  const { match: { params: { category } } } = props
+  const description = categoryDescriptions[category]
+
+  if (!description) {
+    return false
+  }
+  if (category === 'accessories') {
+    return renderHead({
+      description: 'Bridal Accessories in Auckland. Veils and Tiaras',
+      keywords: 'Wedding,Accessories,Veil,Tiara',
+      title: `Xian Bridal: ${description}`,
+    })
+  }
+  return renderHead({
+    description: 'Wedding Gowns in Auckland. Bridesmaid Dresses, Flower Girl Dresses and Ball Gowns',
+    keywords: 'Wedding,Dress,Gown,Auckland,New Zealand,Bridesmaid,Flower Girl',
+    title: `Xian Bridal: ${description}`,
   })
 }
 
 const HeadHome = () => (
   renderHead({
     description: 'Wedding Gowns in Auckland. Bridesmaid Dresses, Flower Girl Dresses, Ball Gowns, Bridal Accessories, Veils and Tiaras',
-    keywords: 'Wedding,Dress,Gown,Auckland,New Zealand,Bridesmaid,Flower Girl,Accessories,Veils,Tiaras',
+    keywords: 'Wedding,Dress,Gown,Auckland,New Zealand,Bridesmaid,Flower Girl,Accessories,Veil,Tiara',
     title: 'Xian Bridal: Wedding Dresses Auckland. Bridesmaid, Flower Girl',
   })
 )
@@ -78,11 +125,19 @@ const Head = (props) => {
       <Switch>
         <Route
           component={HeadProduct}
-          path="/search/:query/:id?"
+          path="/search/:query/:id"
+        />
+        <Route
+          component={HeadSearchResult}
+          path="/search/:query"
         />
         <Route
           component={HeadProduct}
-          path="/:category/:id?"
+          path="/:category/:id"
+        />
+        <Route
+          component={HeadCategory}
+          path="/:category"
         />
         <Route
           component={HeadHome}
